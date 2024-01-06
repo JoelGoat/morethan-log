9bc427866ed5782d2ae324f931ef245840bd5cda
Taekyeom Lee <96657494+taekyeomlee@users.noreply.github.com>
2024-01-06 15:52:44 +0900

    chore: Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "taekyeom",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "tkyeom96@gmail.com",
+    linkedin: "taekyeomlee",
+    github: "taekyeomlee",
     instagram: "",
   },
   projects: [
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "taekyeom-log",
+    description: "welcome to taekyeom-log!",
   },
 
   // CONFIG configration (required)
